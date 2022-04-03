88c0d714361f372578bde2872b167564b26539d4
Eugene Dudkin <ZheDre1N@gmail.com>
2022-04-03 21:43:17 +0300

    part 6 completed

--- PR-Invest/Controllers/WatchListViewController.swift
+++ PR-Invest/Controllers/WatchListViewController.swift
@@ -13,9 +13,36 @@ class WatchListViewController: UIViewController {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
     title = "PR Invest"
+    setUpSearchController()
     // Do any additional setup after loading the view.
   }
 
+  private func setUpSearchController() {
+    let resultVC = SearchResultsViewController()
+    let searchVC = UISearchController(searchResultsController: resultVC)
+    searchVC.searchResultsUpdater = self
+    
+    
+    navigationItem.searchController = searchVC
+  }
 
 }
 
+extension WatchListViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    guard let query = searchController.searchBar.text,
+          !query.trimmingCharacters(in: .whitespaces).isEmpty,
+          let resultVC = searchController.searchResultsController as? SearchResultsViewController
+    else {
+      return
+    }
+    
+    // Optimize search text
+    
+    // Call API to search
+    
+    // update result VC
+    print(query)
+    
+  }
+}
